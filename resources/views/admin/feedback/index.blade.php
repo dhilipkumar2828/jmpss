1964e58439acc268d2327de58fe8d274b804b062
dilip kumar <dilipkumar@oceansoftteam.in>
2026-04-09 18:18:19 +0530

    changes in feedback

--- resources/views/admin/feedback/index.blade.php
+++ resources/views/admin/feedback/index.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 <div class="card">
     <div class="card-header">
-        <h3>Customer Feedbacks</h3>
+        <h3>Parents Feedback</h3>
     </div>
     <div class="card-body">
         <div class="table-wrap">
